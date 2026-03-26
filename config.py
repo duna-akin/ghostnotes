aa4e6b595f128a028a675d882ff6db0656d7341c
Tuna Akin <akint@lafayette.edu>
2026-03-26 14:40:03 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,22 @@
+# file for creating the .ghostnotes configuration
+import configparser
+import os
+
+def create_config():
+    if not os.path.isdir('.git'):
+        print("This project is not initialized with git. GhostNotes only works for directories already initialized with git.")
+        return
+
+    else:
+        # set-up the default config
+        config = configparser.ConfigParser()
+        config['settings'] = {'tag': 'GN:'}
+        config['languages'] = {'.py': '#', '.java': '//', '.js': '//', '.ts': '//'}
+
+        with open('.ghostnotes', 'w') as configfile:
+            config.write(configfile)
+
+        print("GhostNotes: Config file is created succesfully")
+
+def update_exclude():
+    pass
